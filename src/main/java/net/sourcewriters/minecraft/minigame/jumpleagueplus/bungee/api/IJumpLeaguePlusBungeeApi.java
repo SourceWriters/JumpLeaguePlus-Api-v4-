0a2330606acca0cf3b761d2775bc607e1b47dc1d
Lauriichan <laura.endress@syntaxphoenix.com>
2022-04-05 15:19:19 +0200

    Update IJumpLeaguePlusBungeeApi.java

--- src/main/java/net/sourcewriters/minecraft/minigame/jumpleagueplus/bungee/api/IJumpLeaguePlusBungeeApi.java
+++ src/main/java/net/sourcewriters/minecraft/minigame/jumpleagueplus/bungee/api/IJumpLeaguePlusBungeeApi.java
@@ -2,14 +2,21 @@ package net.sourcewriters.minecraft.minigame.jumpleagueplus.bungee.api;
 
 import java.util.concurrent.CompletableFuture;
 
+import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.config.ServerInfo;
 
 public interface IJumpLeaguePlusBungeeApi {
 
     INetFuture<Boolean> isJumpLeagueServer(ServerInfo server);
 
+    CompletableFuture<ServerInfo[]> getAllJumpLeagueServers();
+    
     INetFuture<IBungeeGameInfo> getGameInfo(ServerInfo server);
 
-    CompletableFuture<IBungeeGameInfo[]> getKnownServers();
+    CompletableFuture<IBungeeGameInfo[]> getAllGameInfos();
+
+    static IJumpLeaguePlusBungeeApi getApi() {
+        return (IJumpLeaguePlusBungeeApi) ProxyServer.getInstance().getPluginManager().getPlugin("JumpLeaguePlus");
+    }
 
 }
